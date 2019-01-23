6b108724fde30ddc688019e1bdb85d4d92ea310d
Eddy <rahmanieddy@gmail.com>
2019-01-23 19:07:06 +0100

    Ajout WS recherche article

--- irate/screens/ScanScreen.js
+++ irate/screens/ScanScreen.js
@@ -9,7 +9,8 @@ export default class ScanScreen extends Component {
   state = {
     hasCameraPermission: null,
     lastScanned: "message test",
-    searchingItem: false
+    searchingItem: false,
+    article: null
   };
 
   async componentDidMount() {
@@ -19,7 +20,7 @@ export default class ScanScreen extends Component {
   }
 
   render() {
-    const { hasCameraPermission } = this.state;
+    const { hasCameraPermission, article } = this.state;
 
     if (hasCameraPermission === null) {
       return <Text>Requesting for camera permission</Text>;
@@ -34,14 +35,22 @@ export default class ScanScreen extends Component {
           style={styles.preview}
         >
           <ScanBrackets />
-          {this.state.lastScanned !== null && (
+          {this.state.lastScanned !== null && article  && (
             <Overbox>
               {this.state.searchingItem ? (
                 <ActivityIndicator size="large" color="#020202" />
               ) : (
+                <View>
                 <Text style={{ color: "#020202" }}>
-                  {this.state.lastScanned}
+                  Code : {this.state.lastScanned}
                 </Text>
+                <Text style={{ color: "#020202" }}>
+                 {this.state.article.brand}
+              </Text>
+                <Text style={{ color: "#020202" }}>
+                {article.name} 
+              </Text>
+              </View>
               )}
             </Overbox>
           )}
@@ -51,11 +60,26 @@ export default class ScanScreen extends Component {
   }
 
   handleBarCodeScanned = ({ type, data }) => {
-    if (this.state.lastScanned !== data) {
+    this.setState({lastScanned: data});
+    if (!this.state.searchingItem) {
       this.setState({ searchingItem: true });
-      setTimeout(() => {
-        this.setState({ lastScanned: data, searchingItem: false });
-      }, 2500);
+
+        const url = 'http://192.168.1.55:8888/admin_iRate/public/api/searchArticle/' + data;
+        console.log(url)
+        fetch(url)
+          .then((response) => response.json())
+          .then((responseJson) => {
+            console.log(responseJson)
+            
+            this.setState({  article: responseJson, searchingItem: false });
+          })
+      // this.setState({ searchingItem: true });
+      // setTimeout(() => {
+      //   this.setState({ lastScanned: data, searchingItem: false });
+      // }, 2500);
+    } else {
+      this.setState({ searchingItem: false }); 
+
     }
     //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
